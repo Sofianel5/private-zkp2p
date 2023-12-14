fb7ffe3822a2a4f1bf5be75a251cd3b90de31c0c
Sofianel5 <sofianef1110@gmail.com>
2023-12-14 18:56:38 -0500

    bump-solidity-version

--- contracts/contracts/paymasters/MerkleTreeWithHistory.sol
+++ contracts/contracts/paymasters/MerkleTreeWithHistory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
--- contracts/contracts/paymasters/PrivateMerklePaymaster.sol
+++ contracts/contracts/paymasters/PrivateMerklePaymaster.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.18;
 
 import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
