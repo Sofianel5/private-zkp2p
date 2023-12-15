5cc058fd9df10dc6d7d8b9fcdc45d532d51fc0fd
Sofianel5 <sofianef1110@gmail.com>
2023-12-14 20:54:43 -0500

    fixes

--- contracts/contracts/paymasters/PrivateMerklePaymaster.sol
+++ contracts/contracts/paymasters/PrivateMerklePaymaster.sol
@@ -72,8 +72,6 @@ contract PrivateMerklePaymaster is MerkleTreeWithHistory, ReentrancyGuard {
     }
 
     function calculatePublicAmount(int256 _extAmount, uint256 _fee) public pure returns (uint256) {
-        require(_fee < MAX_FEE, "Invalid fee");
-        require(_extAmount > -MAX_EXT_AMOUNT && _extAmount < MAX_EXT_AMOUNT, "Invalid ext amount");
         int256 publicAmount = _extAmount - int256(_fee);
         return (publicAmount >= 0) ? uint256(publicAmount) : FIELD_SIZE - uint256(-publicAmount);
     }
